f352670cef1d5d68c890fc0aa70d1689f53f04e7
Philippe F <phil.fremy@free.fr>
2018-08-22 22:38:35 +0200

    Indent with spaces instead of tab

--- toto/toto.py
+++ toto/toto.py
@@ -1,26 +1,28 @@
 
-
-
 def add(*args):
-	ret = args[0]
-	for v in args:
-		ret += v
-	return v
+    ret = args[0]
+    for v in args:
+        ret += v
+    return v
 
 def add2(v1, v2):
-	return v1+v2
+    return v1+v2
 
+def main():
+    print( add(1,2,3) )
+    print( add([1,2], [3,4]) )
+    print( add((1,2), (3,4)) )
+
+    print( add2(1,2) )
+    print( add2([1,2], [3,4]) )
+    print( add2((1,2), (3,4)) )
 
 if __name__ == '__main__':
-	from pyannotate_runtime import collect_types
-	collect_types.init_types_collection()
-	with collect_types.collect():
-	    print( add(1,2,3) )
-	    print( add([1,2], [3,4]) )
-	    print( add((1,2), (3,4)) )
-
-	    print( add2(1,2) )
-	    print( add2([1,2], [3,4]) )
-	    print( add2((1,2), (3,4)) )
-
-	collect_types.dump_stats('type_info.json') 
+    if 1:
+        from pyannotate_runtime import collect_types
+        collect_types.init_types_collection()
+        with collect_types.collect():
+            main()
+        collect_types.dump_stats('type_info.json') 
+    else:
+        main()
